9704e543c968213bece9bdc359b29e56f978be33
Alexander Mobaraki <alexandermobaraki@live.se>
2024-10-02 00:35:51 +0200

    Update OpenWeatherApi.py
    Removed unncessecary notes, to improve readibility of the code.

--- OpenWeatherApi.py
+++ OpenWeatherApi.py
@@ -12,7 +12,7 @@ class WeatherAPI:
         """
         # API key for OpenWeatherMap
         self.api_key: str = "d79f1ea93bd9d707f0623a1fe394953b"
-        # Base URL for the current weather APID
+        # Base URL for the current weather API
         self.weather_url: str = "http://api.openweathermap.org/data/2.5/weather"
         # Base URL for the forecast API
         self.forecast_url: str = "http://api.openweathermap.org/data/2.5/forecast"
@@ -27,24 +27,19 @@ class WeatherAPI:
         Returns:
             dict: A dictionary containing weather data, or None if there's an error.
         """
-        # Constructing URL
         url: str = f"{self.weather_url}?q={city}&appid={self.api_key}&units=metric"
     
-        # Error handling: using try-except block
         try:
-            # Making API request
             response: requests.Response = requests.get(url)
         
             # Note: Debugging print, checking if response from API is 200 
             # print("Response status code:", response.status_code)
 
-            # Checking response status
             if response.status_code == 200:
                 full_data: dict = response.json()
                 # Note: This converts the response data from JSON format into a Python dict
                 # Note: By converting the response data to a Dict we can easly access keys in the main code
 
-                # Extract and process the weather data
                 weather_data: dict = {
                     "temperature": float(round(full_data["main"]["temp"], 1)),  # Rounding temperature to 1 decimal place
                     "humidity": int(full_data["main"]["humidity"]),
@@ -60,7 +55,6 @@ class WeatherAPI:
                 
                 return weather_data
             else:
-                # if request failed, print error and return None
                 print(f"Error: Unable to fetch weather data. Status code: {response.status_code}")
                 return None
 
@@ -80,24 +74,21 @@ class WeatherAPI:
         Returns:
             list: A list of dictionaries containing weather data for 5 days, or None if there's an error.
         """
-        # Creating URL
-        url: str = f"{self.forecast_url}?q={city}&appid={self.api_key}&units=metric"
         
-        # Error handling: using try-except block
+        url: str = f"{self.forecast_url}?q={city}&appid={self.api_key}&units=metric"
+    
         try:
-            # Making API request
             response: requests.Response = requests.get(url)
             
             # Note: Debugging print
             # print("Response status code:", response.status_code)
 
-            # Checking response status
+            
             if response.status_code == 200:
                 full_data: dict = response.json()
                 # Note: This converts the response data from JSON format into a Python dict
                 # Note: By converting the response data to a Dict we can easily access keys in the main code
                 
-                # Extract and process the forecast data
                 forecast_data: list = []
                 for item in full_data['list'][::8]: 
                     # Note: Get data for every 24 hours, stepping by 8 
@@ -121,14 +112,12 @@ class WeatherAPI:
                         "wind_speed": float(item['wind']['speed'])
                     })
                 
-                return forecast_data[:5]  # Return data for 5 days
+                return forecast_data[:5]  
             else:
-                # If request failed, basic error handling
                 print(f"Error: Unable to fetch forecast data. Status code: {response.status_code}")
                 return None
 
         except:
-            # Handling any unexpected errors
             print("An error occurred while trying to fetch the forecast data.")
             print("Please check your internet connection and try again.")
             return None
